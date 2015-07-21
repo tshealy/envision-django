850b2d95aa8536518286af322a5df611974fa359
Tripp Shealy <tripp.shealy@gmail.com>
2015-07-21 17:09:40 -0400

    added heroku files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn atusapi.wsgi --log-file
--- envision/envision/wsgi.py
+++ envision/envision/wsgi.py
@@ -1,8 +1,6 @@
 """
-WSGI config for envision project.
-
+WSGI config for atusapi project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
@@ -14,3 +12,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "envision.settings")
 
 application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
